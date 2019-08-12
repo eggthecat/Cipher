ea9303e543a70fa04366e9d1a805ce771bb69162
dominic montelongo and nikhil batta <nikhil@>
2019-08-12 15:51:50 -0700

    Add uppercase function to first and last letter t user input on js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
-
+//
+// $(document).ready(function(){
+//   $("input").submit(function(){
+// var firstName = $(".firstName").val()
+// var lastName = $(".lastName").val()
+// var sentence = (firstName, lastName)
+//
+//  function firstLetter(firstName){
+//
+//  var firstLetter1 = firstName.charAt(0).toUpperCase();
+//  var process =
+// console.log(0);
+// console.log (firstName);
+// }
+// });
+// });
 $(document).ready(function(){
-  $("input").click(function(){
+var sentence = prompt("Hey whats your sentence")
+var sentence1 = sentence.charAt(0).toUpperCase();
+var sentence2 = sentence.slice(1,sentence.length-1);
+var sentence3 = sentence.charAt(sentence.length-1).toUpperCase();
 
-  var firstName = $(".firstName").val();
-
-// var firstName = document.getElementsByClassName(".firstName")
-
-var lastName = $(".lastName").val();
-
-var sentence = (firstName, lastName)
-
-console.log(firstName,lastName)
-});
+//});
+console.log(sentence1 + sentence2 + sentence3)
+console.log()
 });
