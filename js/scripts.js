0a2407398e173acfacaffde3933474067465ed5e
dominic montelongo and nikhil batta <nikhil@>
2019-08-12 14:36:47 -0700

    Add print function to js

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+  $("input").click(function(){
+
+  var firstName = $(".firstName").val();
+
+// var firstName = document.getElementsByClassName(".firstName")
+
+var lastName = $(".lastName").val();
+
+var sentence = (firstName, lastName)
+
+console.log(firstName,lastName)
+});
+});
